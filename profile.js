3f22470d5e8938ba5997cfca8ba1f9c6a24f34c5
Madi <madi_gaziz@mail.ru>
2023-09-11 19:20:58 +0600

    Обновление кода: Сделал дизайн сайта(пока что временный) для проверок всех необходимых проверок. Сделал проверку profile, вместо append изменил на html для замены данных

--- profile.js
+++ profile.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
+    const userEmail = 'john.doe@gmail.com';
+    const apiUrl = `http://146.185.154.90:8000/blog/${userEmail}/profile`;
     $.ajax({
-        url: `http://146.185.154.90:8000/blog/{john.doe@gmail.com}/profile`,
+        url: apiUrl,
         method: 'GET',
         success: function(profile) {
             console.log(profile)
             const username = $('#user_name');
             const email = $('#email')
-            username.append(`${profile.lastName} ${profile.firstName}`)
-            email.append(`${profile.email}`)
-            console.log(username)
+            username.html(`${profile.lastName} ${profile.firstName}`)
+            if (profile) {
+                username.html(`${profile.lastName} ${profile.firstName}`);
+                email.html(`${profile.email}`)
+            } else {
+                console.error('Профиль не найден.');
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error('Произошла ошибка:', status, error);
         }
     })
     $.ajax({
