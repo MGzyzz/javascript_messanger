0a7ae3c5c62f74939ce173cdb9249abca5f2a091
Madi <madi_gaziz@mail.ru>
2023-09-11 18:46:42 +0600

    Обновление кода: определяем все js файлы в один html файл, так же идет проверка на работоспособность новой ветки

--- ribbon.js
+++ ribbon.js
@@ -1,8 +1,5 @@
 $.ajax({
     url: "http://146.185.154.90:8000/blog/john.doe@gmail.com/posts",
 }).then(data => {
-    data = data.slice(-20)
-    for(i of data){
-        console.log(i.user.name, i.message )
-    }
+    console.log(data.slice(-20))
 })
