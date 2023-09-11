8be110f4d36056b93e1d2e0bf25309214c195d1e
Madi <madi_gaziz@mail.ru>
2023-09-11 17:40:16 +0600

    #1 обновление кода: идет подготовка запроса об подпсчиков для показа счетчика на сайте

--- profile/profile.js
+++ profile/profile.js
@@ -1,1 +1,22 @@
-console.log('Hello World!')
+$(document).ready(function() {
+    $.ajax({
+        url: `http://146.185.154.90:8000/blog/{john.doe@gmail.com}/profile`,
+        method: 'GET',
+        success: function(profile) {
+            console.log(profile)
+            const username = $('#user_name');
+            const email = $('#email')
+            username.append(`${profile.lastName} ${profile.firstName}`)
+            email.append(`${profile.email}`)
+            console.log(username)
+        }
+    })
+    $.ajax({
+        url: 'http://146.185.154.90:8000/blog/john.doe@gmail.com/subscribe',
+        method: 'GET',
+        data: [],
+        success: function(subscribe) {
+            console.log(subscribe)
+        }
+    })
+})
