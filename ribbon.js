4cdf405a1916e2a1b07bf25d5510520239d08d3d
Сайденов Рамзан <rama050102@gmail.com>
2023-09-12 19:44:32 +0600

    сделал ленту с обновлением 5 секунд

---
+++ ribbon.js
@@ -0,0 +1,47 @@
+function pageUpdate() {
+    $.ajax({
+        url: "http://146.185.154.90:8000/blog/john.doe@gmail.com/posts",
+    }).then(data => {
+        data = data.slice(-20)
+        post = $('.container-post').empty()
+        console.log(data)
+        for(let i = data.length-1; i >= 0; i--){
+            card = $("<div class='post'>")
+            card.append(
+                `<p class="post-name">${i} ${data[i].user.email} said:</p>
+                <p class="post-content">${data[i].message
+                }</p>`
+                )
+    
+            post.append(card)
+        }
+    }) 
+}
+
+
+const intervalId = setInterval(
+    pageUpdate(),5000)
+
+
+$("#message-form").submit(function(event) {
+    event.preventDefault();
+
+    var formData = {
+        message: $("#message").val()
+    };
+
+    $.ajax({
+        type: "POST",
+        url: "http://146.185.154.90:8000/blog/john.doe@gmail.com/posts",
+        data: formData,
+        dataType: "json",
+        encode: true
+    })
+    .then(()=> {
+        pageUpdate()
+        $("#message").val('') 
+    })
+    .fail(function(error) {
+        alert("Ошибка при отправке запроса: " + JSON.stringify(error));
+    });
+});
--- ribbon/profile.js
+++
@@ -1,8 +0,0 @@
-$.ajax({
-    url: "http://146.185.154.90:8000/blog/john.doe@gmail.com/posts",
-}).then(data => {
-    data = data.slice(-20)
-    for(i of data){
-        console.log(i.user.name, i.message )
-    }
-})
